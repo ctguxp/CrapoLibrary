09cb0658d9db58414146893ed8452259c920e701
Mark Mohr <markm11@charter.net>
2014-05-10 17:30:24 -0400

    Updated to derive from ICollection. Fixed issue where virtual functions where not public. Added function Add(Object*, Object*).

--- corlib/System.Collections.IDictonary.h
+++ corlib/System.Collections.IDictonary.h
@@ -1,13 +1,18 @@
 #pragma once
+#include "System.Collections.ICollection.h"
 
 namespace System
   {
   namespace Collections
     {
-    class IDictionary
-     {
-      virtual bool IsFixedSize() = 0;
-      virtual bool IsReadOnly() = 0;  
+    class IDictionary : public ICollection
+      {
+      public:
+        // Properties
+        virtual bool IsFixedSize() = 0;
+        virtual bool IsReadOnly() = 0;
+        // Functions
+        virtual void Add(Object* key, Object* value) = 0;
       };
     }
   }
