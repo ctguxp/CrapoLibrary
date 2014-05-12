9b586f57d6728d02f4a45b728ce9ab8292ef4017
Mark Mohr <markm11@charter.net>
2014-05-12 14:56:05 -0400

    Added Clear, Contains and Remove virtual functions

--- corlib/System.Collections.IDictonary.h
+++ corlib/System.Collections.IDictonary.h
@@ -13,6 +13,9 @@ namespace System
         virtual bool IsReadOnly() = 0;
         // Functions
         virtual void Add(Object* key, Object* value) = 0;
+        virtual void Clear() = 0;
+        virtual bool Contains(Object* key) = 0;
+        virtual void Remove(Object* key) = 0;
       };
     }
   }
