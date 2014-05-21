23850ad3bd722dbe93321ff6e8377c5adbd48723
Mark Mohr <mark.mohr@ats-global.com>
2014-05-21 14:25:49 -0400

    Can write float, int64 and String now.

--- corlib/System.IO.BinaryWriter.cpp
+++ corlib/System.IO.BinaryWriter.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "System.IO.BinaryWriter.h"
 #include "System.Text.UTF8Encoding.h"
+#include "Crapo.Security.BitConverterLE.h"
+
+using namespace Crapo;
 
 namespace System
   {
@@ -40,6 +43,62 @@ namespace System
       _buffer[2] = (byte) (value >> 16);
       _buffer[3] = (byte) (value >> 24);
       _stream->Write(_buffer, 0, 4);
+      }
+    void BinaryWriter::Write(int64 value)
+      {
+      //if (disposed)
+      //throw new ObjectDisposedException ("BinaryWriter", "Cannot write to a closed BinaryWriter");
+
+      for(int i = 0, sh = 0; i < 8; i++, sh += 8)
+        _buffer[i] = (byte)(value >> sh);
+      _stream->Write(_buffer, 0, 8);
+      }
+    void BinaryWriter::Write(float value)
+      {
+      //if (disposed)
+      //throw new ObjectDisposedException ("BinaryWriter", "Cannot write to a closed BinaryWriter");
+      ByteArray bytes = Security::BitConverterLE::GetBytes(value);
+      _stream->Write(bytes, 0, 4);
+      }
+    void BinaryWriter::Write(String value)
+      {
+
+      //if (disposed)
+      //throw new ObjectDisposedException ("BinaryWriter", "Cannot write to a closed BinaryWriter");
+
+      int32 len = _encoding->GetByteCount(value);
+      Write7BitEncodedInt(len);
+
+      ByteArray stringBuffer(512);
+      int maxCharsPerRound = 512 / _encoding->GetMaxByteCount (1);
+
+      int chpos = 0;
+      int chrem = value.Length();
+      while(chrem > 0) 
+        {
+        int cch = (chrem > maxCharsPerRound) ? maxCharsPerRound : chrem;
+        int blen = _encoding->GetBytes(value, chpos, cch, stringBuffer, 0);
+        _stream->Write(stringBuffer, 0, blen);
+
+        chpos += cch;
+        chrem -= cch;
+        }
+      }
+    void BinaryWriter::Write7BitEncodedInt(int32 value)
+      {
+      do
+        {
+        int32 high = (value >> 7) & 0x01ffffff;
+        byte b = (byte)(value & 0x7f);
+
+        if (high != 0) 
+          {
+          b = (byte)(b | 0x80);
+          }
+
+        Write(b);
+        value = high;
+        }while(value != 0);
       }
     }
   }
--- corlib/System.IO.BinaryWriter.h
+++ corlib/System.IO.BinaryWriter.h
@@ -2,6 +2,7 @@
 #include "System.IO.Stream.h"
 #include "System.Text.Encoding.h"
 #include "System.Array.h"
+#include "System.String.h"
 
 namespace System
   {
@@ -14,7 +15,12 @@ namespace System
         virtual ~BinaryWriter();
         virtual void Write(bool /*value*/);
         virtual void Write(byte /*value*/);
+        virtual void Write(float /*value*/);
         virtual void Write(int32 /*value*/);
+        virtual void Write(int64 /*value*/);
+        virtual void Write(String /*value*/);
+      protected:
+        void Write7BitEncodedInt(int32 /*value*/);
       private:
         bool             _leaveOpen;
         ByteArray        _buffer;
