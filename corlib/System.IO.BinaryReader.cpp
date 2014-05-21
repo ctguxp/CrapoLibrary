d946599671db3265bbf2e28d3bd83acfc24ca34f
Mark Mohr <mark.mohr@ats-global.com>
2014-05-21 14:25:13 -0400

    Added ReadSingle, ReadInt64, ReadString functions.

--- corlib/System.IO.BinaryReader.cpp
+++ corlib/System.IO.BinaryReader.cpp
@@ -3,7 +3,12 @@
 #include "System.Text.UTF8Encoding.h"
 #include "System.Exception.h"
 #include "System.IO.IOException.h"
+#include "System.FormatException.h"
 #include "System.Math.h"
+#include "System.Text.StringBuilder.h"
+#include "Crapo.Security.BitConverterLE.h"
+
+using namespace Crapo;
 
 namespace System
   {
@@ -15,7 +20,7 @@ namespace System
       ,_stream(stream)
       ,_encoding(new Text::UTF8Encoding())
       {
-      _buffer.Length(Math::Min(16, _encoding->GetMaxByteCount(1)));
+      _buffer.Length(Math::Max(16, _encoding->GetMaxByteCount(1)));
       }
     BinaryReader::BinaryReader(Stream* stream, Text::Encoding* encoding)
       :_leaveOpen(false)
@@ -26,7 +31,7 @@ namespace System
       if(_encoding.Get() != nullptr)
         {
         _encoding.RescindOwnership();
-        _buffer.Length(Math::Min(16, _encoding->GetMaxByteCount(1)));
+        _buffer.Length(Math::Max(16, _encoding->GetMaxByteCount(1)));
         }
       }
     BinaryReader::~BinaryReader()
@@ -60,7 +65,71 @@ namespace System
 
       return(_buffer[0] | (_buffer[1] << 8) | (_buffer[2] << 16) | (_buffer[3] << 24));
       }
-    void BinaryReader::FillBuffer(int numBytes)
+    int64 BinaryReader::ReadInt64()
+      {
+      FillBuffer(8);
+
+      uint32 ret_low  = (uint32) (_buffer[0] | (_buffer[1] << 8) | (_buffer[2] << 16) | (_buffer[3] << 24));
+
+      uint32 ret_high = (uint32) (_buffer[4] | (_buffer[5] << 8) | (_buffer[6] << 16) | (_buffer[7] << 24));
+
+      return (int64) ((((uint64) ret_high) << 32) | ret_low);
+      }
+    float BinaryReader::ReadSingle()
+      {
+      FillBuffer(4);
+
+      return(Security::BitConverterLE::ToSingle(_buffer, 0));
+      }
+    String BinaryReader::ReadString() 
+      {
+      /* Inspection of BinaryWriter-written files
+      * shows that the length is given in bytes,
+      * not chars
+      */
+      int32 len = Read7BitEncodedInt();
+
+      if(len < 0)
+        throw new IOException(L"Invalid binary file (string len < 0)");
+
+      if(len == 0)
+        return String::Empty();
+
+      CharArray charBuffer(_encoding->GetMaxByteCount(MaxBufferSize));
+      ByteArray charByteBuffer(MaxBufferSize);
+
+      //
+      // We read the string here in small chunks. Also, we
+      // Attempt to optimize the common case of short strings.
+      //
+      using namespace Text;
+      GCStringBuilder sb;
+      do 
+        {
+        int32 readLen = Math::Min(MaxBufferSize, len);
+
+        readLen = _stream->Read(charByteBuffer, 0, readLen);
+        if(readLen == 0)
+          throw SystemException(L"End of Stream");
+        //TODO throw new EndOfStreamException();
+
+        int cch = _encoding->GetDecoder()->GetChars(charByteBuffer, 0, readLen, charBuffer, 0);
+
+        if(sb.Get() == nullptr && readLen == len) // ok, we got out the easy way, dont bother with the sb
+          return String(charBuffer.ToConstPtr(), 0, cch);
+
+        if(sb.Get() == nullptr)
+          // Len is a fairly good estimate of the number of chars in a string
+            // Most of the time 1 byte == 1 char
+              sb.Set(new StringBuilder(len));
+
+        sb->Append(charBuffer, 0, cch);
+        len -= readLen;
+        } while (len > 0);
+
+        return sb->ToString();
+      }
+    void BinaryReader::FillBuffer(sizet numBytes)
       {
       if(numBytes > _buffer.Length())
         throw ArgumentOutOfRangeException(L"numBytes");
@@ -70,11 +139,11 @@ namespace System
         throw IOException(L"Stream is invalid");
 
       /* Cope with partial reads */
-      int pos=0;
+      sizet pos=0;
 
-      while(pos<numBytes)
+      while(pos < numBytes)
         {
-        int n= _stream->Read(_buffer, pos, numBytes-pos);
+        int n = _stream->Read(_buffer, (int)pos, (int)(numBytes-pos));
         if(n==0)
           {
           throw SystemException(L"End of Stream");
@@ -83,6 +152,28 @@ namespace System
 
         pos+=n;
         }
+      }
+    int BinaryReader::Read7BitEncodedInt() 
+      {
+      int32 ret = 0;
+      int32 shift = 0;
+      int32 len;
+      byte b;
+
+      for(len = 0; len < 5; ++len) 
+        {
+        b = ReadByte();
+
+        ret = ret | ((b & 0x7f) << shift);
+        shift += 7;
+        if ((b & 0x80) == 0)
+          break;
+        }
+
+      if (len < 5)
+        return ret;
+      else
+        throw FormatException(L"Too many bytes in what should have been a 7 bit encoded Int32.");
       }
     }
   }
--- corlib/System.IO.BinaryReader.h
+++ corlib/System.IO.BinaryReader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "System.IO.Stream.h"
 #include "System.Text.Encoding.h"
+#include "System.String.h"
 
 namespace System
   {
@@ -8,15 +9,23 @@ namespace System
     {
     class CRAPOCOREDLL_API BinaryReader
       {
+      enum
+        {
+        MaxBufferSize = 128
+        };
       public:
         BinaryReader(Stream* /*stream*/);
         BinaryReader(Stream* /*stream*/, Text::Encoding* /*encoding*/);
         virtual ~BinaryReader();
         virtual bool ReadBoolean();
         virtual byte ReadByte();
+        virtual float ReadSingle();
         virtual int32 ReadInt32();
+        virtual int64 ReadInt64();
+        virtual String ReadString();
       protected:
-        virtual void FillBuffer(int /*numBytes*/);
+        virtual void FillBuffer(sizet /*numBytes*/);
+        int Read7BitEncodedInt();
       private:
         bool             _leaveOpen;
         ByteArray        _buffer;
