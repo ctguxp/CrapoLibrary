5e39d0cf9d41eade0e2cb968c00ddd262f63c2b9
Mark Mohr <markm11@charter.net>
2014-05-21 11:44:14 -0400

    Added ExpressionCollection not ready to use.

---
+++ System/System.Text.RegularExpressions.Syntax.ExpressionCollection.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "System.Text.RegularExpressions.Syntax.ExpressionCollection.h"
+
+namespace System
+  {
+  namespace Text
+    {
+    namespace RegularExpressions
+      {
+      ExpressionCollection::ExpressionCollection()
+        {
+        }
+      ExpressionCollection::~ExpressionCollection()
+        {
+        }
+      }
+    }
+  }
---
+++ System/System.Text.RegularExpressions.Syntax.ExpressionCollection.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace System
+  {
+  namespace Text
+    {
+    namespace RegularExpressions
+      {
+      class ExpressionCollection : public Collections::CollectionBase
+        {
+        public:
+          ExpressionCollection();
+          ~ExpressionCollection();
+        };
+      }
+    }
+  }
