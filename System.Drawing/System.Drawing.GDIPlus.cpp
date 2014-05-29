60c436aa78d79f70fa17f40e9aca83f5e8ef3d5f
Mark Mohr <mark.mohr@ats-global.com>
2014-05-29 17:30:04 -0400

    Now using DllImport and dynamic loading functions from gdiplus.dll

--- System.Drawing/System.Drawing.GDIPlus.cpp
+++ System.Drawing/System.Drawing.GDIPlus.cpp
@@ -1,32 +1,66 @@
 #include "pch.h"
 #include "System.Drawing.GDIPlus.h"
 
-using namespace Gdiplus;
-
 namespace System
   {
+  using namespace Runtime;
   namespace Drawing
     {
+
+    ULONG_PTR GDIPlus::_gdiplusToken = 0;
+    AutoPtr<GdiplusStartupInput>        GDIPlus::_startInput(new GdiplusStartupInput());
+    AutoPtr<GdiplusStartupOutput>       GDIPlus::_startOutput(new GdiplusStartupOutput());
+    AutoPtr<InteropServices::DllImport> GDIPlus::_gdiDll(new InteropServices::DllImport(L"gdiplus.dll"));
+
     GDIPlus::GDIPlus()
-      :_gdiplusToken()
-      ,_startInput()
-      ,_startOutput()
       {
-      _startInput.Set(new GdiplusStartupInput());
       _startInput->GdiplusVersion = 1;
       _startInput->DebugEventCallback = nullptr;
       _startInput->SuppressBackgroundThread = FALSE;
       _startInput->SuppressExternalCodecs = FALSE;
 
-      _startOutput.Set(new GdiplusStartupOutput());
       _startOutput->NotificationHook = nullptr;
       _startOutput->NotificationUnhook = nullptr;
 
-      GdiplusStartup(&_gdiplusToken, _startInput.Get(), _startOutput.Get());
+      Startup startup;
+      _gdiDll->GetFunction(L"GdiplusStartup", startup);
+      startup(&_gdiplusToken, _startInput.Get(), _startOutput.Get());
       }
     GDIPlus::~GDIPlus()
       {
-      GdiplusShutdown(_gdiplusToken);
+      Shutdown shutdown;
+      _gdiDll->GetFunction(L"GdiplusShutdown", shutdown);
+      shutdown(_gdiplusToken);
+      }
+    int GDIPlus::GdipCreateFromHDC(HDC hdc, void** graphics)
+      {
+      CreateFromHDC create;
+      _gdiDll->GetFunction(L"GdipCreateFromHDC", create);
+      return create(hdc, graphics);
+      }
+    int GDIPlus::GdipCreatePen1(DWORD argb, float width, int unit, void** pen)
+      {
+      CreatePen1 pen1;
+      _gdiDll->GetFunction(L"GdipCreatePen1", pen1);
+      return pen1(argb, width, unit, pen);
+      }
+    int GDIPlus::GdipDeleteGraphics(void* graphics)
+      {
+      DeleteGraphics delGraphics;
+      _gdiDll->GetFunction(L"GdipDeleteGraphics", delGraphics);
+      return delGraphics(graphics);
+      }
+    int GDIPlus::GdipDeletePen(void* pen)
+      {
+      DeletePen delPen;
+      _gdiDll->GetFunction(L"GdipDeletePen", delPen);
+      return delPen(pen);
+      }
+    int GDIPlus::GdipDrawLineI(void* graphics, void* pen, int x1, int y1, int x2, int y2)
+      {
+      DrawLineI draw;
+      _gdiDll->GetFunction(L"GdipDrawLineI", draw);
+      return draw(graphics, pen, x1, y1, x2, y2);
       }
     }
   }
--- System.Drawing/System.Drawing.GDIPlus.h
+++ System.Drawing/System.Drawing.GDIPlus.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "System.Runtime.InteropServices.DllImport.h"
 
 namespace System
   {
@@ -6,13 +7,27 @@ namespace System
     {
     class GDIPlus
       {
+      private:
+        static ULONG_PTR                                    _gdiplusToken;
+        static AutoPtr<GdiplusStartupInput>                 _startInput;
+        static AutoPtr<GdiplusStartupOutput>                _startOutput;
+        static AutoPtr<Runtime::InteropServices::DllImport> _gdiDll;
       public:
         GDIPlus();
         ~GDIPlus();
-      protected:
-        ULONG_PTR                              _gdiplusToken;
-        AutoPtr<Gdiplus::GdiplusStartupInput>  _startInput;
-        AutoPtr<Gdiplus::GdiplusStartupOutput> _startOutput;
+        static int GdipCreateFromHDC(HDC /*hdc*/, void** /*graphics*/);
+        static int GdipCreatePen1(DWORD argb, float width, int unit, void** pen);
+        static int GdipDeleteGraphics(void* /*graphics*/);
+        static int GdipDeletePen(void* /*pen*/);
+        static int GdipDrawLineI(void* /*graphics*/, void* /*pen*/, int /*x1*/, int /*y1*/, int /*x2*/, int /*y2*/);
+      private:
+        typedef int (FAR PASCAL* Startup)(ULONG_PTR* token, const GdiplusStartupInput* input, GdiplusStartupOutput* output);
+        typedef void (FAR PASCAL* Shutdown)(ULONG_PTR token);
+        typedef int (FAR PASCAL* CreateFromHDC)(HDC hdc, void** graphics);
+        typedef int (FAR PASCAL* CreatePen1)(DWORD color, float width, int unit, void **pen);
+        typedef int (FAR PASCAL* DeleteGraphics)(void* graphics);
+        typedef int (FAR PASCAL* DeletePen)(void* pen);
+        typedef int (FAR PASCAL* DrawLineI)(void* graphics, void* pen, int x1, int y1, int x2, int y2);
       };
     }
   }
