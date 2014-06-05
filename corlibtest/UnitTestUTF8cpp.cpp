148d6e12d738e012f97c078d2d46aee7efdb891a
Mark Mohr <markm11@charter.net>
2014-06-05 08:20:08 -0400

    Fixed format problem

--- corlibtest/UnitTestUTF8cpp.cpp
+++ corlibtest/UnitTestUTF8cpp.cpp
@@ -5,38 +5,38 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace System;
 
 namespace corlibtest
-{
-	TEST_CLASS(UnitTestUTF8cpp)
-	{
-	public:
-		
-		TEST_METHOD(ByteCount)
-		{
-    Text::UTF8Encoding enc;
-    String duffy(L"Duffy is my dog");
-    int n = enc.GetByteCount(duffy);
-    Assert::AreEqual<int>(15, n);
-		}
+  {
+  TEST_CLASS(UnitTestUTF8cpp)
+    {
+    public:
 
-    TEST_METHOD(GetBytesTelegu)
-		{
-    try
-      {
-      Text::UTF8Encoding enc;
-      String duffy(L"అధ్యక్షుడు హియర్");
-      ByteArray n = enc.GetBytes(duffy);
-      Assert::AreEqual<sizet>(46, n.Length());
-      int c = enc.GetCharCount(n, 0, (int)n.Length());
-      Assert::AreEqual<int>(16, c);
-      }
-    catch(Exception& ex)
-      {
-      Assert::Fail(ex.Message());
-      }
-    catch(std::exception ex)
-      {
-      Assert::Fail(L"Standard Exception Thrown");
-      }
-		}
-	};
-}
+      TEST_METHOD(ByteCount)
+        {
+        Text::UTF8Encoding enc;
+        String duffy(L"Duffy is my dog");
+        int n = enc.GetByteCount(duffy);
+        Assert::AreEqual<int>(15, n);
+        }
+
+      TEST_METHOD(GetBytesTelegu)
+        {
+        try
+          {
+          Text::UTF8Encoding enc;
+          String duffy(L"అధ్యక్షుడు హియర్");
+          ByteArray n = enc.GetBytes(duffy);
+          Assert::AreEqual<sizet>(46, n.Length());
+          int c = enc.GetCharCount(n, 0, (int)n.Length());
+          Assert::AreEqual<int>(16, c);
+          }
+        catch(Exception& ex)
+          {
+          Assert::Fail(ex.Message());
+          }
+        catch(std::exception ex)
+          {
+          Assert::Fail(L"Standard Exception Thrown");
+          }
+        }
+    };
+  }
