25d0ef5bb4a5b32eab907be2fe6d78d85a972d80
Mark Mohr <mark.mohr@ats-global.com>
2014-06-07 13:04:18 -0400

    Added IsFixedSize, IsReadOnly, Add and Contains interface functions

--- corlib/System.Collections.IList.h
+++ corlib/System.Collections.IList.h
@@ -8,7 +8,14 @@ namespace System
     class IList : public ICollection
       {
       public:
+        // Properties
+        virtual bool IsFixedSize() = 0;
+		    virtual bool IsReadOnly() = 0;
+
+        // Functions
+        virtual int Add(Object* value) = 0;
         virtual void Clear() = 0;
+        virtual bool Contains(Object* value) = 0;
       };
     }
   }
