01eb376ed86b27e62787390db1720bc04905edc4
Mark Mohr <mark.mohr@ats-global.com>
2014-06-07 13:07:57 -0400

    Fixed _default pointer with AutoPtr; Implemented more code needs more coding.

--- corlib/System.Collections.Generic.EqualityComparer.h
+++ corlib/System.Collections.Generic.EqualityComparer.h
@@ -15,37 +15,54 @@ namespace System
           class DefaultComparer : public EqualityComparer<T>
             {
             public:
-              virtual int GetHashCode (T obj) override
+              virtual int GetHashCode(T& obj) override
                 {
                 return obj.GetHashCode();
                 }
-              virtual bool Equals (T x, T y)
+              virtual bool Equals(T& x, T& y) override
                 {
                 return x.Equals(&y);
                 }
             };
         public:
-          EqualityComparer()
+          virtual ~EqualityComparer()
             {
             }
-          virtual bool Equals(T, T) override
+          virtual bool Equals(T&, T&) override
             {
             return false;
             }
-          virtual int GetHashCode(T) override
+          virtual int GetHashCode(T&) override
             {
             return 0;
             }
           static EqualityComparer<T>* Default()
             {
-            return _default;
+            if(_default.Get() == nullptr)
+              _default.Set(new EqualityComparer<T>::DefaultComparer());
+            return _default.Get();
+            }
+        protected:
+          EqualityComparer()
+            {
+            }
+          virtual int IndexOf(Array<T*> arr, T& value, int startIndex, int endIndex)
+            {
+            for(int i = startIndex; i < endIndex; ++i)
+              {
+              if(Equals(*arr[i], value))
+                return i;
+              }
+            return -1;
             }
         private:
-          static EqualityComparer<T>* _default;
+          static AutoPtr<EqualityComparer<T>> _default;
+
+        friend class Array<T>;
         };
 
       template<class T>
-      EqualityComparer<T>* EqualityComparer<T>::_default = new EqualityComparer<T>::DefaultComparer();
+      AutoPtr<EqualityComparer<T>> EqualityComparer<T>::_default;
       }
     }
   }
