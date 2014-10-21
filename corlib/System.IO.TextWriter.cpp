c63379e88c7cef8d2f159733a131a12adaf09106
Mark Mohr <mark.mohr@ats-global.com>
2014-10-21 16:52:12 -0400

    Updated for unit test and adding Write and WriteLine functions.  Still have more to do.

--- corlib/System.IO.TextWriter.cpp
+++ corlib/System.IO.TextWriter.cpp
@@ -1,15 +1,131 @@
 #include "pch.h"
 #include "System.IO.TextWriter.h"
+#include "System.h"
 
 namespace System
   {
   namespace IO
     {
     TextWriter::TextWriter()
+      :_coreNewLine(Environment::NewLine().ToCharArray())
+      ,_newLine(_coreNewLine.ToConstPtr())
+      ,_internalFormatProvider(nullptr)
+      {
+      }
+    TextWriter::TextWriter(IFormatProvider* formatProvider)
+      :_coreNewLine(Environment::NewLine().ToCharArray())
+      ,_newLine(_coreNewLine.ToConstPtr())
+      ,_internalFormatProvider(formatProvider)
       {
       }
     TextWriter::~TextWriter()
       {
       }
+    String& TextWriter::NewLine()
+      {
+      return _newLine = _coreNewLine.ToConstPtr();
+      }
+
+    void TextWriter::NewLine(String& value)
+      {
+      if(value.Length() == 0)
+        _coreNewLine = Environment::NewLine().ToCharArray();
+
+      _coreNewLine = value.ToCharArray();
+      }
+
+    void TextWriter::Write(bool value)
+      {
+      Boolean b(value);
+      Write(b.ToString());
+      }
+
+    void TextWriter::Write(int32 value)
+      {
+      Int32 i(value);
+      Write(i.ToString(_internalFormatProvider));
+      }
+
+    void TextWriter::Write(int64 value)
+      {
+      Int64 i(value);
+      Write(i.ToString(_internalFormatProvider));
+      }
+
+    void TextWriter::Write(double value)
+      {
+      Double d(value);
+      Write(d.ToString(_internalFormatProvider));
+      }
+
+    void TextWriter::Write(float value)
+      {
+      Single s(value);
+      Write(s.ToString(_internalFormatProvider));
+      }
+
+    void TextWriter::Write(wchar_t /*value*/)
+      {
+      // Do nothing
+      }
+
+    void TextWriter::Write(String value)
+      {
+      if(value.Length() != 0)
+        Write(value.ToCharArray());
+      }
+
+    void TextWriter::Write(Object* value)
+      {
+      if(value != nullptr)
+        Write(value->ToString());
+      }
+
+    void TextWriter::Write(CharArray buffer)
+      {
+      if(buffer.IsNull())
+        return;
+      Write(buffer, 0, buffer.Length());
+      }
+
+    void TextWriter::Write(CharArray buffer, int32 index, int32 count)
+      {
+      if(buffer.IsNull())
+        throw ArgumentNullException(L"buffer");
+      if(index < 0 || index > (int32)buffer.Length())
+        throw ArgumentOutOfRangeException(L"index");
+      // re-ordered to avoid possible integer overflow
+      if(count < 0 || (index > (int32)buffer.Length() - count))
+        throw ArgumentOutOfRangeException(L"count");
+
+      for(; count > 0; --count, ++index)
+        {
+        Write(buffer[index]);
+        }
+      }
+
+    void TextWriter::WriteLine()
+      {
+      Write(_coreNewLine);
+      }
+
+    void TextWriter::WriteLine(bool value)
+      {
+      Write(value);
+      WriteLine();
+      }
+
+    void TextWriter::WriteLine(wchar_t value)
+      {
+      Write(value);
+      WriteLine ();
+      }
+
+    void TextWriter::WriteLine(CharArray buffer)
+      {
+      Write(buffer);
+      WriteLine ();
+      }
+
     }
   }
--- corlib/System.IO.TextWriter.h
+++ corlib/System.IO.TextWriter.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "System.Array.h"
+#include "System.String.h"
+#include "System.IFormatProvider.h"
 
 namespace System
   {
@@ -7,9 +10,30 @@ namespace System
     class CRAPOCOREDLL_API TextWriter
       {
       public:
-        ~TextWriter();
+        virtual ~TextWriter();
+        virtual String& NewLine();
+        virtual void NewLine(String& /*value*/);
+        virtual void Write(bool /*value*/);
+        virtual void Write(double /*value*/);
+        virtual void Write(float /*value*/);
+        virtual void Write(int32 /*value*/);
+        virtual void Write(int64 /*value*/);
+        virtual void Write(wchar_t /*value*/);
+        virtual void Write(String /*value*/);
+        virtual void Write(Object* /*value*/);
+        virtual void Write(CharArray /*buffer*/);
+        virtual void Write(CharArray /*buffer*/, int32 /*index*/, int32 /*count*/);
+        virtual void WriteLine();
+        virtual void WriteLine(bool /*value*/);
+        virtual void WriteLine(wchar_t /*value*/);
+        virtual void WriteLine(CharArray /*buffer*/);
       protected:
         TextWriter();
+        TextWriter(IFormatProvider* /*formatProvider*/);
+      protected:
+        CharArray        _coreNewLine;
+        String           _newLine;
+        IFormatProvider* _internalFormatProvider;
       };
     }
   }
