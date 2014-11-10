0c1a23b921545ca19fabed23e064335946b3fd67
Mark Mohr <mark.mohr@ats-global.com>
2014-11-10 12:22:00 -0500

    Added System.Globalization.h

---
+++ corlib/System.Globalization.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "System.Globalization.CompareInfo.h"
+#include "System.Globalization.CompareOptions.h"
+#include "System.Globalization.CultureInfo.h"
+#include "System.Globalization.NumberFormatInfo.h"
+#include "System.Globalization.NumberStyles.h"
+#include "System.Globalization.TextInfo.h"
+#include "System.Globalization.UnicodeCategory.h"
